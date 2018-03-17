2fac0059fb6535ceca986c445be6be836bee50f9
Ricardo <ricardosr.mota@gmail.com>
2018-03-16 22:51:11 -0300

    Altercao

--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,9 +1,14 @@
+import {Professor} from './professor.model';
 export class Disciplina {
   nome: string;
   descricao: string;
+  professor: Professor;
+  codigo: number;
 
-  constructor(nome: string, descricao?: string) {
+  constructor(codigo: number, nome: string, descricao?: string, professor?: Professor) {
     this.nome = nome;
     this.descricao = descricao;
+    this.professor = professor;
+    this.codigo = codigo;
   }
 }
